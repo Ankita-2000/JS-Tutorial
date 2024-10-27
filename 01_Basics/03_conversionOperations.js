66bd719aaea9faedfe8eece205971329232c7431
Ankita Saraswat <71912517+Ankita-2000@users.noreply.github.com>
2024-10-27 02:48:34 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+// let score = "true";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); //number even if the value passed is not a number ex. "33abc"
+// console.log(valueInNumber);  // NaN not a number
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Ankita" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
