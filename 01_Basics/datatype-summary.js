a8fa90175233f4932746880dc078d9c3ac3d1156
Ankita Saraswat <71912517+Ankita-2000@users.noreply.github.com>
2024-11-09 05:52:43 +0000

    datatype-summary

---
+++ 01_Basics/datatype-summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+const value = 33
+const valueNum = 33.5
+
+const temp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
+
+const bigNumber = 3456789012345667889n
+
+
+// Reference
+
+const heros = ["shaktiman", "naagraj", "doga"];   //ARRAYS
+
+
+//** OBJECTS **//
+let myObj = {
+    name: "Ankita",
+    age: 24,
+}
+
+
+//FUNCTIONS//
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myFunction);
+
